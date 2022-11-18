1abc57e70af3b835038912b36ed40d4df3f86776
betacarrot <zrenah@connect.ust.hk>
2022-11-18 15:57:46 +0800

    select mesh

--- src.js
+++ src.js
@@ -26,42 +26,19 @@ const material = new THREE.ShaderMaterial({
 
 const params = {
     fabric_type: 1,
-    light: 15.0
+    light: 15.0,
+    mesh: 'bunny'
 }
 
 gui.add(params, 'fabric_type', { linen: 0, silk: 1, polyester_front: 2, polyester_back: 3, cross_color_silk: 4, velvet: 5 });
 gui.add(params, 'light', 0, 30);
+gui.add(params, 'mesh', { bunny: 'bunny', cactus: 'cactus', curtain: 'curtain', pillow: 'pillow', sy: 'sy' });
 
-const loader = new OBJLoader();
 let mesh;
-loader.load(
-    // resource URL
-    'models/sy.obj',
-    // called when resource is loaded
-    function (obj) {
-        mesh = obj.children[0];
-        mesh.material = material;
-        mesh.geometry.deleteAttribute("normal");
-        console.log(mesh.geometry);
-        mesh.geometry = mergeVertices(mesh.geometry);
-        mesh.geometry.computeVertexNormals();
-        console.log(mesh.geometry);
-        scene.add(mesh);
-    },
-    // called when loading is in progresses
-    function (xhr) {
+const loader = new OBJLoader();
+loadMesh();
 
-    },
-    // called when loading has errors
-    function (error) {
-
-        console.log(error);
-
-    }
-);
-
-camera.position.z = 5;
-
+let lastMesh = 'bunny';
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -71,10 +48,49 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+camera.position.z = 5;
+
+function loadMesh() {
+    scene.clear();
+    loader.load(
+        // resource URL
+        'models/' + params.mesh + '.obj',
+        // called when resource is loaded
+        function (obj) {
+            mesh = obj.children[0];
+            mesh.material = material;
+            mesh.geometry.deleteAttribute("normal");
+            mesh.geometry = mergeVertices(mesh.geometry);
+            mesh.geometry.computeVertexNormals();
+            scene.add(mesh);
+            controls.reset();
+            camera.position.z = 5;
+        },
+        // called when loading is in progresses
+        function (xhr) {
+
+        },
+        // called when loading has errors
+        function (error) {
+
+            console.log(error);
+
+        }
+    );
+
+}
+
 function animate() {
     requestAnimationFrame(animate);
-    mesh.material.uniforms.type.value = params.fabric_type;
-    mesh.material.uniforms.light.value = params.light;
+    if (typeof mesh !== 'undefined') {
+        mesh.material.uniforms.type.value = params.fabric_type;
+        mesh.material.uniforms.light.value = params.light;
+        if (lastMesh != params.mesh) {
+            console.log("change");
+            loadMesh();
+            lastMesh = params.mesh;
+        }
+    }
     renderer.render(scene, camera);
 };
 
